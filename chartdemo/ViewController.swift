8b5a854810b36894ccd1391a5435e198c0fbc527
Gregori Faroux <gregori@Le-MacBook.local>
2014-10-30 22:44:13 -0400

    Initial Commit

---
+++ chartdemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chartdemo
+//
+//  Created by Gregori Faroux on 10/30/14.
+//  Copyright (c) 2014 digistarters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
